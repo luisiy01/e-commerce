9d526d81de44d3f4ef77739f8001681f737f40d4
Luis Nunez <luisnunez91@gmail.com>
2026-04-21 15:50:56 -0600

    agregar endoint de pago a strapi

--- ecommerce-strapi/src/api/order/controllers/order.ts
+++ ecommerce-strapi/src/api/order/controllers/order.ts
@@ -3,12 +3,59 @@
  */
 
 import { factories } from "@strapi/strapi";
+import Stripe from "stripe";
+
+const calcDiscountPrice = (price, discount) => {
+  const discountAmount = (price * discount) / 100;
+  const result = price - discountAmount;
+  return result.toFixed(2);
+};
 
 export default factories.createCoreController(
   "api::order.order",
   ({ strapi }) => ({
     async paymentOrder(ctx) {
-      ctx.body = "pago generado correctamente";
+      const { token, products, idUser, addressShipping } = ctx.request.body;
+
+      let totalPayment = 0;
+      products.forEach((product) => {
+        const priceDiscount = calcDiscountPrice(
+          product.price,
+          product.discount,
+        );
+        totalPayment += Number(priceDiscount) * product.quantity;
+      });
+
+      const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
+        apiVersion: "2026-03-25.dahlia",
+      });
+
+      const paymentIntent = await stripe.charges.create({
+        amount: Math.round(totalPayment * 100),
+        currency: "usd",
+        source: token.id,
+        description: `User Id: ${idUser}`,
+      });
+
+      const data = {
+        products,
+        user: idUser,
+        totalPayment,
+        idPayment: paymentIntent.id,
+        addressShipping,
+      };
+
+      const model = strapi.contentType("api::order.order");
+      const validData = await strapi.entityValidator.validateEntityCreation(
+        model,
+        data as any,
+      );
+
+      const entry = await strapi.db
+        .query("api::order.order")
+        .create({ data: validData });
+
+      return entry;
     },
   }),
 );
