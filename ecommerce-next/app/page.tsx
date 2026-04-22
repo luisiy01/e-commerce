06a42ef57b119e177e88e89b10a66d681e10b94d
Luis Nunez <luisnunez91@gmail.com>
2026-04-22 10:32:55 -0600

    actualizar page

--- ecommerce-next/app/page.tsx
+++ ecommerce-next/app/page.tsx
@@ -1,12 +1,10 @@
 import Button from "@mui/material/Button";
-import ButtonDemo from "./components/ButtonDemo/ButtonDemo";
 
 export default function Home() {
   return (
     <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
         <Button variant="contained">Hello world</Button>
-        <ButtonDemo />
       </main>
     </div>
   );
